68cb3ac43b47624e10d963eb463b3ff48cae6018
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-13 23:26:46 +1200

    Add infrastructure for functional tests

---
+++ test/PetDoctor.API.Tests.Functional/Setup/TestFixture.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+
+namespace PetDoctor.API.Tests.Functional.Setup
+{
+    public class TestFixture
+    {
+        private readonly TestApiFactory _webApplicationFactory;
+        public HttpClient Client { get; }
+
+        public TestFixture()
+        {
+            _webApplicationFactory = new TestApiFactory();
+            Client = _webApplicationFactory.CreateClient();
+        }
+    }
+}
---
+++ test/PetDoctor.API.Tests.Functional/Setup/TestStartup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PetDoctor.Infrastructure;
+using SqlStreamStore;
+using System;
+
+namespace PetDoctor.API.Tests.Functional.Setup
+{
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        protected override void ConfigureDatabaseServices(IServiceCollection services)
+        {
+            services.AddEntityFrameworkInMemoryDatabase();
+            services.AddDbContext<PetDoctorContext>(options => options.UseInMemoryDatabase($"petdoc-{Guid.NewGuid()}"));
+            services.AddSingleton<IStreamStore, InMemoryStreamStore>();
+        }
+    }
+}
