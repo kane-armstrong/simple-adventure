0914a09aa22e2e17ef751daa0aec91908c610ed3
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-14 01:59:24 +1200

    Add missed checkin of test infrastructure

---
+++ test/PetDoctor.API.Tests.Functional/Setup/TestApiFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
+
+namespace PetDoctor.API.Tests.Functional.Setup
+{
+    public sealed class TestApiFactory : WebApplicationFactory<Startup>
+    {
+        protected override IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<TestStartup>();
+                });
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var applicationPath = Path.GetFullPath(PlatformServices.Default.Application.ApplicationBasePath);
+            builder.UseContentRoot(applicationPath).UseEnvironment("Development");
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
