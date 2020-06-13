e86293e60c8a7946687a31db28430de2e010e64b
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-14 01:37:06 +1200

    Fix create appointment tests (real db broke them)

--- test/PetDoctor.API.Tests.Functional/Controllers/AppointmentController/CreateAppointmentSpec/A_successful_create_appointment_request.cs
+++ test/PetDoctor.API.Tests.Functional/Controllers/AppointmentController/CreateAppointmentSpec/A_successful_create_appointment_request.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using PetDoctor.API.Application.Commands;
+using PetDoctor.API.Tests.Functional.Helpers;
 using PetDoctor.API.Tests.Functional.Setup;
 using System;
 using System.Linq;
@@ -11,52 +12,60 @@ using Xunit;
 
 namespace PetDoctor.API.Tests.Functional.Controllers.AppointmentController.CreateAppointmentSpec
 {
-    public class A_successful_create_appointment_request : IClassFixture<TestFixture>
+    [Collection(TestCollections.RealDatabaseTests)]
+    public class A_successful_create_appointment_request
     {
         private const string EndpointRoute = "api/v1/appointments";
 
         private readonly TestFixture _testFixture;
+        private readonly Fixture _fixture;
 
         public A_successful_create_appointment_request(TestFixture testFixture)
         {
             _testFixture = testFixture;
+            _fixture = new Fixture();
+            _fixture.Customize(new CreateAppointmentCustomization());
         }
 
         [Fact]
+        [ResetDatabase]
         public async Task returns_201_created()
         {
             var client = _testFixture.Client;
-            var fixture = new Fixture();
-            var request = fixture.Create<CreateAppointment>();
+            var request = _fixture.Create<CreateAppointment>();
 
-            var result = await client.PostAsJsonAsync(EndpointRoute, request);
+            var response = await client.PostAsJsonAsync(EndpointRoute, request);
+            await response.ThrowWithBodyIfUnsuccessfulStatusCode();
 
-            result.StatusCode.Should().Be(StatusCodes.Status201Created);
+            response.StatusCode.Should().Be(StatusCodes.Status201Created);
         }
 
         [Fact]
+        [ResetDatabase]
         public async Task returns_the_id_of_created_appointment_in_location_header()
         {
             var client = _testFixture.Client;
-            var fixture = new Fixture();
-            var request = fixture.Create<CreateAppointment>();
+            var request = _fixture.Create<CreateAppointment>();
 
             var response = await client.PostAsJsonAsync(EndpointRoute, request);
+            await response.ThrowWithBodyIfUnsuccessfulStatusCode();
 
-            response.IsSuccessStatusCode.Should().BeTrue();
             var expectedLocationPattern = $"{client.BaseAddress}{EndpointRoute}/\\d";
             response.Headers.Location.AbsoluteUri.Should().MatchRegex(expectedLocationPattern);
         }
 
         [Fact]
+        [ResetDatabase]
         public async Task actually_persists_the_appointment()
         {
             var client = _testFixture.Client;
-            var fixture = new Fixture();
-            var request = fixture.Create<CreateAppointment>();
 
-            var result = await client.PostAsJsonAsync(EndpointRoute, request);
-            var foundIdInLocationHeader = Guid.TryParse(result.Headers.Location.AbsoluteUri.Split('/').Last(), out var id);
+            var request = _fixture.Create<CreateAppointment>();
+
+            var response = await client.PostAsJsonAsync(EndpointRoute, request);
+            await response.ThrowWithBodyIfUnsuccessfulStatusCode();
+
+            var foundIdInLocationHeader = Guid.TryParse(response.Headers.Location.AbsoluteUri.Split('/').Last(), out var id);
             foundIdInLocationHeader.Should().BeTrue();
 
             var created = await _testFixture.FindAppointment(id);
