bb823be6ca506fb85dfa97eb20f54a5579e73238
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-13 15:03:37 +1200

    Expand replay events test cases

--- test/PetDoctor.Tests.Unit/Domain/Aggregates/Appointments/AppointmentSpec/Replaying_events.cs
+++ test/PetDoctor.Tests.Unit/Domain/Aggregates/Appointments/AppointmentSpec/Replaying_events.cs
@@ -39,6 +39,98 @@ namespace PetDoctor.Tests.Unit.Domain.Aggregates.Appointments.AppointmentSpec
 
         public static IEnumerable<object[]> TestEvents()
         {
+            yield return CreateCompletedAppointment();
+            yield return CreateCompletedRescheduledAppointment();
+            yield return CreateRejectedAppointment();
+            yield return CreateRejectedRescheduledAppointment();
+            yield return CreateCanceledAppointment();
+        }
+
+        private static object[] CreateCompletedAppointment()
+        {
+            var fixture = new Fixture();
+            var createdEvent = fixture.Create<AppointmentCreated>();
+
+            var appointment = new Appointment(createdEvent);
+
+            var vetId = Guid.NewGuid();
+
+            var events = new List<DomainEvent>
+            {
+                new AppointmentConfirmed(appointment.Id, vetId),
+                new AppointmentMembersCheckedIn(appointment.Id),
+                new AppointmentCompleted(appointment.Id)
+            };
+
+            appointment.Confirm(vetId);
+            appointment.CheckIn();
+            appointment.Complete();
+
+            return new object[]
+            {
+                createdEvent, events, appointment
+            };
+        }
+
+        private static object[] CreateCompletedRescheduledAppointment()
+        {
+            var fixture = new Fixture();
+            var createdEvent = fixture.Create<AppointmentCreated>();
+
+            var appointment = new Appointment(createdEvent);
+
+            var firstVetId = Guid.NewGuid();
+            var secondVetId = Guid.NewGuid();
+            var newDate = appointment.ScheduledOn.AddDays(3);
+
+            var events = new List<DomainEvent>
+            {
+                new AppointmentConfirmed(appointment.Id, firstVetId),
+                new AppointmentRescheduled(appointment.Id, newDate),
+                new AppointmentConfirmed(appointment.Id, secondVetId),
+                new AppointmentMembersCheckedIn(appointment.Id),
+                new AppointmentCompleted(appointment.Id)
+            };
+
+            appointment.Confirm(firstVetId);
+            appointment.Reschedule(newDate);
+            appointment.Confirm(secondVetId);
+            appointment.CheckIn();
+            appointment.Complete();
+
+            return new object[]
+            {
+                createdEvent, events, appointment
+            };
+        }
+
+        private static object[] CreateRejectedAppointment()
+        {
+            var fixture = new Fixture();
+            var createdEvent = fixture.Create<AppointmentCreated>();
+
+            var appointment = new Appointment(createdEvent);
+
+            var vetId = Guid.NewGuid();
+            const string reason = "no";
+
+            var events = new List<DomainEvent>
+            {
+                new AppointmentConfirmed(appointment.Id, vetId),
+                new AppointmentRejected(appointment.Id, reason)
+            };
+
+            appointment.Confirm(vetId);
+            appointment.Reject(reason);
+
+            return new object[]
+            {
+                createdEvent, events, appointment
+            };
+        }
+
+        private static object[] CreateRejectedRescheduledAppointment()
+        {
             var fixture = new Fixture();
             var createdEvent = fixture.Create<AppointmentCreated>();
 
@@ -46,23 +138,45 @@ namespace PetDoctor.Tests.Unit.Domain.Aggregates.Appointments.AppointmentSpec
 
             var vetId = Guid.NewGuid();
             var newDate = appointment.ScheduledOn.AddDays(3);
+            const string reason = "no";
 
             var events = new List<DomainEvent>
             {
-                new AppointmentConfirmed(appointment.Id, Guid.NewGuid()),
-                new AppointmentRescheduled(appointment.Id, newDate),
                 new AppointmentConfirmed(appointment.Id, vetId),
-                new AppointmentMembersCheckedIn(appointment.Id),
-                new AppointmentCompleted(appointment.Id)
+                new AppointmentRescheduled(appointment.Id, newDate),
+                new AppointmentRejected(appointment.Id, reason)
             };
 
             appointment.Confirm(vetId);
             appointment.Reschedule(newDate);
-            appointment.Confirm(vetId);
-            appointment.CheckIn();
-            appointment.Complete();
+            appointment.Reject(reason);
 
-            yield return new object[]
+            return new object[]
+            {
+                createdEvent, events, appointment
+            };
+        }
+
+        private static object[] CreateCanceledAppointment()
+        {
+            var fixture = new Fixture();
+            var createdEvent = fixture.Create<AppointmentCreated>();
+
+            var appointment = new Appointment(createdEvent);
+
+            var vetId = Guid.NewGuid();
+            var reason = "no money";
+
+            var events = new List<DomainEvent>
+            {
+                new AppointmentConfirmed(appointment.Id, vetId),
+                new AppointmentCanceled(appointment.Id, reason)
+            };
+
+            appointment.Confirm(vetId);
+            appointment.Cancel(reason);
+
+            return new object[]
             {
                 createdEvent, events, appointment
             };
