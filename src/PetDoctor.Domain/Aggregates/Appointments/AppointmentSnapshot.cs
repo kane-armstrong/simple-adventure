732e5aada3162259a00468d36656acfc7ce7ea15
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-13 20:53:21 +1200

    Add missing items to AppointmentSnapshot

--- src/PetDoctor.Domain/Aggregates/Appointments/AppointmentSnapshot.cs
+++ src/PetDoctor.Domain/Aggregates/Appointments/AppointmentSnapshot.cs
@@ -8,7 +8,9 @@ namespace PetDoctor.Domain.Aggregates.Appointments
         public Pet? Pet { get; set; }
         public Owner? Owner { get; set; }
         public Guid? AttendingVeterinarianId { get; set; }
-        public string? ReasonForVisit { get; set; }
+        public string? ReasonForVisit { get; }
+        public string? RejectionReason { get; set; }
+        public string? CancellationReason { get; set; }
         public DateTimeOffset ScheduledOn { get; set; }
         public AppointmentState State { get; set; }
     }
