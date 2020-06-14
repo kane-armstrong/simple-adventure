41ee8fea907b1b6c66e96e6cc7a1788f7acc0489
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-14 13:23:03 +1200

    Implement reschedule appointment validation

---
+++ test/PetDoctor.Tests.Unit/API/Application/Validators/RescheduleAppointmentValidatorSpec/Validation_should_fail_when.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.TestHelper;
+using PetDoctor.API.Application.Commands;
+using PetDoctor.API.Application.Validators;
+using System;
+using Xunit;
+
+namespace PetDoctor.Tests.Unit.API.Application.Validators.RescheduleAppointmentValidatorSpec
+{
+    public class Validation_should_fail_when
+    {
+        [Fact]
+        public void new_date_is_in_the_past()
+        {
+            var request = new RescheduleAppointment
+            {
+                NewDate = DateTimeOffset.UtcNow.AddDays(-1)
+            };
+
+            var sut = new RescheduleAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.NewDate, request);
+        }
+    }
+}
