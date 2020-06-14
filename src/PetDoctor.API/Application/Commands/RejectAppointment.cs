967f1c7d0331db443d48b2cc28e28a54c6ae52c0
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-14 12:34:57 +1200

    Implement reject appointment

--- src/PetDoctor.API/Application/Commands/RejectAppointment.cs
+++ src/PetDoctor.API/Application/Commands/RejectAppointment.cs
@@ -5,5 +5,6 @@ namespace PetDoctor.API.Application.Commands
     public class RejectAppointment : Command
     {
         internal Guid Id { get; set; }
+        public string Reason { get; set; }
     }
 }
---
+++ test/PetDoctor.API.Tests.Functional/Controllers/AppointmentController/RejectAppointmentSpec/A_successful_reject_appointment_request.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using PetDoctor.API.Application.Commands;
+using PetDoctor.API.Tests.Functional.Helpers;
+using PetDoctor.API.Tests.Functional.Setup;
+using PetDoctor.Domain.Aggregates.Appointments;
+using Xunit;
+
+namespace PetDoctor.API.Tests.Functional.Controllers.AppointmentController.RejectAppointmentSpec
+{
+    [Collection(TestCollections.RealDatabaseTests)]
+    public class A_successful_reject_appointment_request
+    {
+        private const string EndpointRoute = "api/v1/appointments";
+
+        private readonly TestFixture _testFixture;
+        private readonly Fixture _fixture;
+
+        public A_successful_reject_appointment_request(TestFixture testFixture)
+        {
+            _testFixture = testFixture;
+            _fixture = new Fixture();
+            _fixture.Customize(new CreateAppointmentCustomization());
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task returns_204_no_content()
+        {
+            var client = _testFixture.Client;
+            var seeder = new AppointmentSeeder();
+            var id = await seeder.CreateAppointment(client);
+            var request = _fixture.Create<RejectAppointment>();
+            var uri = $"{EndpointRoute}/{id}/reject";
+
+            var response = await client.PutAsJsonAsync(uri, request);
+            await response.ThrowWithBodyIfUnsuccessfulStatusCode();
+
+            response.StatusCode.Should().Be(StatusCodes.Status204NoContent);
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task actually_cancels_the_appointment()
+        {
+            var client = _testFixture.Client;
+            var seeder = new AppointmentSeeder();
+            var id = await seeder.CreateAppointment(client);
+            var request = _fixture.Create<RejectAppointment>();
+            var uri = $"{EndpointRoute}/{id}/reject";
+
+            var response = await client.PutAsJsonAsync(uri, request);
+            await response.ThrowWithBodyIfUnsuccessfulStatusCode();
+
+            var sut = await _testFixture.FindAppointment(id);
+            sut.State.Should().Be(AppointmentState.Rejected);
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task captures_the_rejection_reason()
+        {
+            var client = _testFixture.Client;
+            var seeder = new AppointmentSeeder();
+            var id = await seeder.CreateAppointment(client);
+            var request = _fixture.Create<RejectAppointment>();
+            var uri = $"{EndpointRoute}/{id}/reject";
+
+            var response = await client.PutAsJsonAsync(uri, request);
+            await response.ThrowWithBodyIfUnsuccessfulStatusCode();
+
+            var sut = await _testFixture.FindAppointment(id);
+            sut.RejectionReason.Should().Be(request.Reason);
+        }
+    }
+}
