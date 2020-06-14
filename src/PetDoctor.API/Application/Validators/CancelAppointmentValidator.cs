3c36aca07104edc6a4a2065696e732763e708985
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-14 12:51:18 +1200

    Implement cancel appointment validation

--- src/PetDoctor.API/Application/Validators/CancelAppointmentValidator.cs
+++ src/PetDoctor.API/Application/Validators/CancelAppointmentValidator.cs
@@ -5,5 +5,15 @@ namespace PetDoctor.API.Application.Validators
 {
     public class CancelAppointmentValidator : AbstractValidator<CancelAppointment>
     {
+        public CancelAppointmentValidator()
+        {
+            const int maxReasonLength = 1000;
+
+            RuleFor(p => p.Reason)
+                .NotEmpty()
+                .WithMessage($"{nameof(CancelAppointment.Reason)} is required")
+                .MaximumLength(maxReasonLength)
+                .WithMessage($"{nameof(CancelAppointment.Reason)} must not exceed {maxReasonLength} characters in length");
+        }
     }
 }
---
+++ test/PetDoctor.Tests.Unit/API/Application/Validators/CancelAppointmentValidatorSpec/Validation_should_fail_when.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation.TestHelper;
+using PetDoctor.API.Application.Commands;
+using PetDoctor.API.Application.Validators;
+using System.Linq;
+using Xunit;
+
+namespace PetDoctor.Tests.Unit.API.Application.Validators.CancelAppointmentValidatorSpec
+{
+    public class Validation_should_fail_when
+    {
+        [Fact]
+        public void cancellation_reason_is_null()
+        {
+            var request = new CancelAppointment
+            {
+                Reason = null
+            };
+
+            var sut = new CancelAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.Reason, request);
+        }
+
+        [Fact]
+        public void cancellation_reason_is_empty()
+        {
+            var request = new CancelAppointment
+            {
+                Reason = string.Empty
+            };
+
+            var sut = new CancelAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.Reason, request);
+        }
+
+        [Fact]
+        public void cancellation_reason_is_longer_than_1000_characters()
+        {
+            var request = new CancelAppointment
+            {
+                Reason = string.Join("", Enumerable.Range(1, 1001))
+            };
+
+            var sut = new CancelAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.Reason, request);
+        }
+    }
+}
