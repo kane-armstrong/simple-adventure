f5ef4856a3ea3c7a35de57636ca1556c05762116
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-14 13:16:42 +1200

    Implement reject appointment validation

--- src/PetDoctor.API/Application/Validators/RejectAppointmentValidator.cs
+++ src/PetDoctor.API/Application/Validators/RejectAppointmentValidator.cs
@@ -5,5 +5,14 @@ namespace PetDoctor.API.Application.Validators
 {
     public class RejectAppointmentValidator : AbstractValidator<RejectAppointment>
     {
+        public RejectAppointmentValidator()
+        {
+            var maxReasonLength = 1000;
+            RuleFor(p => p.Reason)
+                .NotEmpty()
+                .WithMessage($"{nameof(CancelAppointment.Reason)} is required")
+                .MaximumLength(maxReasonLength)
+                .WithMessage($"{nameof(CancelAppointment.Reason)} must not exceed {maxReasonLength} characters in length");
+        }
     }
 }
---
+++ test/PetDoctor.Tests.Unit/API/Application/Validators/RejectAppointmentValidatorSpec/Validation_should_fail_when.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using FluentValidation.TestHelper;
+using PetDoctor.API.Application.Commands;
+using PetDoctor.API.Application.Validators;
+using Xunit;
+
+namespace PetDoctor.Tests.Unit.API.Application.Validators.RejectAppointmentValidatorSpec
+{
+    public class Validation_should_fail_when
+    {
+        [Fact]
+        public void rejection_reason_is_null()
+        {
+            var request = new RejectAppointment
+            {
+                Reason = null
+            };
+
+            var sut = new RejectAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.Reason, request);
+        }
+
+        [Fact]
+        public void rejection_reason_is_empty()
+        {
+            var request = new RejectAppointment
+            {
+                Reason = string.Empty
+            };
+
+            var sut = new RejectAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.Reason, request);
+        }
+
+        [Fact]
+        public void rejection_reason_is_longer_than_1000_characters()
+        {
+            var request = new RejectAppointment
+            {
+                Reason = string.Join("", Enumerable.Range(1, 1001))
+            };
+
+            var sut = new RejectAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.Reason, request);
+        }
+    }
+}
