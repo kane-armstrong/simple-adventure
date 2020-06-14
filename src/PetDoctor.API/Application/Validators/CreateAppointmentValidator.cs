4edc5e648b109624b70520cf06817f57672c8180
Kane Armstrong <kane.armstrong@outlook.com>
2020-06-14 14:10:32 +1200

    Implement create appointment validation

--- src/PetDoctor.API/Application/Validators/CreateAppointmentValidator.cs
+++ src/PetDoctor.API/Application/Validators/CreateAppointmentValidator.cs
@@ -1,9 +1,77 @@
 ﻿using FluentValidation;
 using PetDoctor.API.Application.Commands;
+using System;
 
 namespace PetDoctor.API.Application.Validators
 {
     public class CreateAppointmentValidator : AbstractValidator<CreateAppointment>
     {
+        public CreateAppointmentValidator()
+        {
+            const int maxReasonLength = 1000;
+            RuleFor(p => p.ReasonForVisit)
+                .NotEmpty()
+                .WithMessage($"{nameof(CreateAppointment.ReasonForVisit)} is required")
+                .MaximumLength(maxReasonLength)
+                .WithMessage($"{nameof(CreateAppointment.ReasonForVisit)} must not exceed {maxReasonLength} characters in length");
+
+            RuleFor(p => p.DesiredDate)
+                .Must(BeInTheFuture)
+                .WithMessage($"{nameof(CreateAppointment.DesiredDate)} must be in the future");
+
+            const int maxOwnerFirstNameLength = 100;
+            RuleFor(p => p.OwnerFirstName)
+                .NotEmpty()
+                .WithMessage($"{nameof(CreateAppointment.OwnerFirstName)} is required")
+                .MaximumLength(maxOwnerFirstNameLength)
+                .WithMessage($"{nameof(CreateAppointment.OwnerFirstName)} must not exceed {maxOwnerFirstNameLength} characters in length");
+
+            const int maxOwnerLastNameLength = 100;
+            RuleFor(p => p.OwnerLastName)
+                .NotEmpty()
+                .WithMessage($"{nameof(CreateAppointment.OwnerLastName)} is required")
+                .MaximumLength(maxOwnerLastNameLength)
+                .WithMessage($"{nameof(CreateAppointment.OwnerLastName)} must not exceed {maxOwnerLastNameLength} characters in length");
+
+            const int maxOwnerEmailLength = 100;
+            RuleFor(p => p.OwnerEmail)
+                .NotEmpty()
+                .WithMessage($"{nameof(CreateAppointment.OwnerEmail)} is required")
+                .MaximumLength(maxOwnerEmailLength)
+                .WithMessage($"{nameof(CreateAppointment.OwnerEmail)} must not exceed {maxOwnerEmailLength} characters in length");
+
+            const int maxOwnerPhoneLength = 25;
+            RuleFor(p => p.OwnerPhone)
+                .NotEmpty()
+                .WithMessage($"{nameof(CreateAppointment.OwnerPhone)} is required")
+                .MaximumLength(maxOwnerPhoneLength)
+                .WithMessage($"{nameof(CreateAppointment.OwnerPhone)} must not exceed {maxOwnerPhoneLength} characters in length");
+
+            const int maxPetNameLength = 100;
+            RuleFor(p => p.PetName)
+                .NotEmpty()
+                .WithMessage($"{nameof(CreateAppointment.PetName)} is required")
+                .MaximumLength(maxPetNameLength)
+                .WithMessage($"{nameof(CreateAppointment.PetName)} must not exceed {maxPetNameLength} characters in length");
+
+            RuleFor(p => p.PetDateOfBirth)
+                .Must(BeInThePast)
+                .WithMessage($"{nameof(CreateAppointment.PetDateOfBirth)} must be in the past");
+
+            const int maxPetBreedLength = 100;
+            RuleFor(p => p.PetBreed)
+                .NotEmpty()
+                .WithMessage($"{nameof(CreateAppointment.PetBreed)} is required")
+                .MaximumLength(maxPetBreedLength)
+                .WithMessage($"{nameof(CreateAppointment.PetBreed)} must not exceed {maxPetBreedLength} characters in length");
+        }
+
+        private static bool BeInTheFuture(DateTimeOffset argument)
+            // DateTimeOffset comparison converts compared values to UTC before doing the comparison, so this is safe
+            => argument > DateTimeOffset.UtcNow;
+
+        private static bool BeInThePast(DateTimeOffset argument)
+            // DateTimeOffset comparison converts compared values to UTC before doing the comparison, so this is safe
+            => argument < DateTimeOffset.UtcNow;
     }
 }
---
+++ test/PetDoctor.Tests.Unit/API/Application/Validators/CreateAppointmentValidatorSpec/Validation_should_fail_when.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using FluentValidation.TestHelper;
+using PetDoctor.API.Application.Commands;
+using PetDoctor.API.Application.Validators;
+using System.Linq;
+using Xunit;
+
+namespace PetDoctor.Tests.Unit.API.Application.Validators.CreateAppointmentValidatorSpec
+{
+    public class Validation_should_fail_when
+    {
+        [Fact]
+        public void reason_for_visit_is_null()
+        {
+            var request = new CreateAppointment
+            {
+                ReasonForVisit = null
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.ReasonForVisit, request);
+        }
+
+        [Fact]
+        public void reason_for_visit_is_empty()
+        {
+            var request = new CreateAppointment
+            {
+                ReasonForVisit = string.Empty
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.ReasonForVisit, request);
+        }
+
+        [Fact]
+        public void reason_for_visit_is_longer_than_1000_characters()
+        {
+            var request = new CreateAppointment
+            {
+                ReasonForVisit = string.Join("", Enumerable.Range(1, 1001))
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.ReasonForVisit, request);
+        }
+
+        [Fact]
+        public void desired_date_is_in_the_past()
+        {
+            var request = new CreateAppointment
+            {
+                DesiredDate = DateTimeOffset.UtcNow.AddDays(-1)
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.DesiredDate, request);
+        }
+
+        [Fact]
+        public void owner_first_name_is_null()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerFirstName = null
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerFirstName, request);
+        }
+
+        [Fact]
+        public void owner_first_name_is_empty()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerFirstName = string.Empty
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerFirstName, request);
+        }
+
+        [Fact]
+        public void owner_first_name_is_longer_than_100_characters()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerFirstName = string.Join("", Enumerable.Range(1, 101))
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerFirstName, request);
+        }
+
+        [Fact]
+        public void owner_last_name_is_null()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerLastName = null
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerLastName, request);
+        }
+
+        [Fact]
+        public void owner_last_name_is_empty()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerLastName = string.Empty
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerLastName, request);
+        }
+
+        [Fact]
+        public void owner_last_name_is_longer_than_100_characters()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerLastName = string.Join("", Enumerable.Range(1, 101))
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerLastName, request);
+        }
+        
+        [Fact]
+        public void owner_email_is_null()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerEmail = null
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerEmail, request);
+        }
+
+        [Fact]
+        public void owner_email_is_empty()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerEmail = string.Empty
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerEmail, request);
+        }
+
+        [Fact]
+        public void owner_email_is_longer_than_100_characters()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerEmail = string.Join("", Enumerable.Range(1, 101))
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerEmail, request);
+        }
+
+        [Fact]
+        public void owner_phone_is_null()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerPhone = null
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerPhone, request);
+        }
+
+        [Fact]
+        public void owner_phone_is_empty()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerPhone = string.Empty
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerPhone, request);
+        }
+
+        [Fact]
+        public void owner_phone_is_longer_than_25_characters()
+        {
+            var request = new CreateAppointment
+            {
+                OwnerPhone = string.Join("", Enumerable.Range(1, 26))
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.OwnerPhone, request);
+        }
+
+        [Fact]
+        public void pet_name_is_null()
+        {
+            var request = new CreateAppointment
+            {
+                PetName = null
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.PetName, request);
+        }
+
+        [Fact]
+        public void pet_name_is_empty()
+        {
+            var request = new CreateAppointment
+            {
+                PetName = string.Empty
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.PetName, request);
+        }
+
+        [Fact]
+        public void pet_name_is_longer_than_100_characters()
+        {
+            var request = new CreateAppointment
+            {
+                PetName = string.Join("", Enumerable.Range(1, 101))
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.PetName, request);
+        }
+        
+        [Fact]
+        public void pet_breed_is_null()
+        {
+            var request = new CreateAppointment
+            {
+                PetBreed = null
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.PetBreed, request);
+        }
+
+        [Fact]
+        public void pet_breed_is_empty()
+        {
+            var request = new CreateAppointment
+            {
+                PetBreed = string.Empty
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.PetBreed, request);
+        }
+
+        [Fact]
+        public void pet_breed_is_longer_than_100_characters()
+        {
+            var request = new CreateAppointment
+            {
+                PetBreed = string.Join("", Enumerable.Range(1, 101))
+            };
+
+            var sut = new CreateAppointmentValidator();
+
+            sut.ShouldHaveValidationErrorFor(p => p.PetBreed, request);
+        }
+    }
+}
