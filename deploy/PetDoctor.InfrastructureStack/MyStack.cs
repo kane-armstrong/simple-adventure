388e220694ea70a889e3b3ff7da29df72a1af547
Kane Armstrong <kane.armstrong@outlook.com>
2020-07-20 08:49:00 +1200

    Add skeleton project for Pulumi

---
+++ deploy/PetDoctor.InfrastructureStack/MyStack.cs
@@ -0,0 +1,8 @@
+﻿using Pulumi;
+
+namespace PetDoctor.InfrastructureStack
+{
+    public class MyStack : Stack
+    {
+    }
+}
---
+++ deploy/PetDoctor.InfrastructureStack/Program.cs
@@ -0,0 +1,10 @@
+﻿using Pulumi;
+using System.Threading.Tasks;
+
+namespace PetDoctor.InfrastructureStack
+{
+    class Program
+    {
+        static Task<int> Main() => Deployment.RunAsync<MyStack>();
+    }
+}
