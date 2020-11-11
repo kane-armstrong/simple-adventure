410f4a01b5e27f0b929c38949b382ed7ef1190cf
Kane Armstrong <kane.armstrong@outlook.com>
2020-11-11 21:28:56 +1300

    Fix failed tests; something wrong with this record

--- src/PetDoctor.Domain/Aggregates/Appointments/Events/AppointmentCreated.cs
+++ src/PetDoctor.Domain/Aggregates/Appointments/Events/AppointmentCreated.cs
@@ -2,7 +2,15 @@
 
 namespace PetDoctor.Domain.Aggregates.Appointments.Events
 {
-    public record AppointmentCreated(Guid AppointmentId, AppointmentMemento Data) : DomainEvent
+    public record AppointmentCreated : DomainEvent
     {
+        public Guid AppointmentId { get; }
+        public AppointmentMemento Data { get; }
+
+        public AppointmentCreated(Guid appointmentId, AppointmentMemento data)
+        {
+            AppointmentId = appointmentId;
+            Data = data;
+        }
     }
 }
