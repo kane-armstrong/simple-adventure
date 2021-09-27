f1ed4371b69b04034d087c7c7e326649647abb8f
Kane Armstrong <kane.armstrong@outlook.com>
2021-09-27 16:25:50 +1300

    Move things to facilitate adding more deployables

--- build-docker.ps1
+++
@@ -1,8 +0,0 @@
-Param(
-    [Parameter(Mandatory=$True, Position=0)]
-    [System.String] $Version
-)
-
-docker build -t petdoctor/petdoctor-api .
-docker tag petdoctor/petdoctor-api kanearmstrongdev.azurecr.io/petdoctor/petdoctor-api:$Version
-docker push kanearmstrongdev.azurecr.io/petdoctor/petdoctor-api:$Version
