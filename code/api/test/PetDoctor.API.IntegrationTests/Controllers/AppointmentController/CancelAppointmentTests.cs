d44eca5d58ae13886142246b6c11e9a5d51c2cd8
Kane Armstrong <kane.armstrong@outlook.com>
2021-11-13 00:05:48 +1300

    Revise appointment cancellation tests

---
+++ code/api/test/PetDoctor.API.IntegrationTests/Controllers/AppointmentController/CancelAppointmentTests.cs
@@ -0,0 +1,93 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using PetDoctor.API.Application.Commands;
+using PetDoctor.API.IntegrationTests.Helpers;
+using PetDoctor.API.IntegrationTests.Setup;
+using PetDoctor.Domain.Aggregates.Appointments;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PetDoctor.API.IntegrationTests.Controllers.AppointmentController;
+
+[Collection(TestCollections.RealDatabaseTests)]
+public class CancelAppointmentTests
+{
+    private const string EndpointRoute = "v1/appointments";
+
+    private readonly TestFixture _testFixture;
+    private readonly Fixture _fixture;
+
+    public CancelAppointmentTests(TestFixture testFixture)
+    {
+        _testFixture = testFixture;
+        _fixture = new Fixture();
+        _fixture.Customize(new CreateAppointmentCustomization());
+    }
+
+    [Fact]
+    [ResetDatabase]
+    public async Task Canceling_an_appointment_returns_a_204_no_content_response()
+    {
+        var client = _testFixture.Client;
+        var seeder = new AppointmentSeeder();
+        var id = await seeder.CreateAppointment(client);
+        var request = _fixture.Create<CancelAppointment>();
+        var uri = $"{EndpointRoute}/{id}/cancel";
+
+        var response = await client.PutAsJsonAsync(uri, request);
+        await response.ThrowWithBodyIfUnsuccessfulStatusCode();
+
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    [ResetDatabase]
+    public async Task Appointment_cancellations_are_persisted_correctly()
+    {
+        var client = _testFixture.Client;
+        var seeder = new AppointmentSeeder();
+        var id = await seeder.CreateAppointment(client);
+        var request = _fixture.Create<CancelAppointment>();
+        var uri = $"{EndpointRoute}/{id}/cancel";
+
+        var response = await client.PutAsJsonAsync(uri, request);
+        await response.ThrowWithBodyIfUnsuccessfulStatusCode();
+
+        var sut = await _testFixture.FindAppointment(id);
+        sut.State.Should().Be(AppointmentState.Canceled);
+    }
+
+    [Fact]
+    [ResetDatabase]
+    public async Task The_reason_for_cancellation_is_captured_correctly()
+    {
+        var client = _testFixture.Client;
+        var seeder = new AppointmentSeeder();
+        var id = await seeder.CreateAppointment(client);
+        var request = _fixture.Create<CancelAppointment>();
+        var uri = $"{EndpointRoute}/{id}/cancel";
+
+        var response = await client.PutAsJsonAsync(uri, request);
+        await response.ThrowWithBodyIfUnsuccessfulStatusCode();
+
+        var sut = await _testFixture.FindAppointment(id);
+        sut.CancellationReason.Should().Be(request.Reason);
+    }
+
+    [Fact]
+    [ResetDatabase]
+    public async Task Cancelling_an_appointment_fails_with_a_404_not_found_when_the_appointment_does_not_exist()
+    {
+        var client = _testFixture.Client;
+        var id = Guid.NewGuid();
+        var request = _fixture.Create<CancelAppointment>();
+        var uri = $"{EndpointRoute}/{id}/cancel";
+
+        var response = await client.PutAsJsonAsync(uri, request);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
