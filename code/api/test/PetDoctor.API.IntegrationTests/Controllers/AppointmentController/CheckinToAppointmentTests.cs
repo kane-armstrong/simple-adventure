3b0fb520f01936c98f7efe1104e5f933d7adb788
Kane Armstrong <kane.armstrong@outlook.com>
2021-11-13 00:09:09 +1300

    Refactor checkin to appointment tests

---
+++ code/api/test/PetDoctor.API.IntegrationTests/Controllers/AppointmentController/CheckinToAppointmentTests.cs
@@ -0,0 +1,76 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using PetDoctor.API.Application.Commands;
+using PetDoctor.API.IntegrationTests.Helpers;
+using PetDoctor.API.IntegrationTests.Setup;
+using PetDoctor.Domain.Aggregates.Appointments;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PetDoctor.API.IntegrationTests.Controllers.AppointmentController;
+
+[Collection(TestCollections.RealDatabaseTests)]
+public class CheckinToAppointmentTests
+{
+    private const string EndpointRoute = "v1/appointments";
+
+    private readonly TestFixture _testFixture;
+    private readonly Fixture _fixture;
+
+    public CheckinToAppointmentTests(TestFixture testFixture)
+    {
+        _testFixture = testFixture;
+        _fixture = new Fixture();
+        _fixture.Customize(new CreateAppointmentCustomization());
+    }
+
+    [Fact]
+    [ResetDatabase]
+    public async Task Checking_in_to_an_appointment_returns_204_no_content()
+    {
+        var client = _testFixture.Client;
+        var seeder = new AppointmentSeeder();
+        var id = await seeder.CreateAppointment(client);
+        var request = _fixture.Create<CheckinToAppointment>();
+        var uri = $"{EndpointRoute}/{id}/checkin";
+
+        var response = await client.PutAsJsonAsync(uri, request);
+        await response.ThrowWithBodyIfUnsuccessfulStatusCode();
+
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    [ResetDatabase]
+    public async Task Appointment_check_ins_are_persisted_correctly()
+    {
+        var client = _testFixture.Client;
+        var seeder = new AppointmentSeeder();
+        var id = await seeder.CreateAppointment(client);
+        var request = _fixture.Create<CheckinToAppointment>();
+        var uri = $"{EndpointRoute}/{id}/checkin";
+
+        var response = await client.PutAsJsonAsync(uri, request);
+        await response.ThrowWithBodyIfUnsuccessfulStatusCode();
+
+        var sut = await _testFixture.FindAppointment(id);
+        sut.State.Should().Be(AppointmentState.CheckedIn);
+    }
+
+    [Fact]
+    [ResetDatabase]
+    public async Task Checking_into_an_appointment_fails_with_a_404_not_found_when_the_appointment_does_not_exist()
+    {
+        var client = _testFixture.Client;
+        var id = Guid.NewGuid();
+        var request = _fixture.Create<CheckinToAppointment>();
+        var uri = $"{EndpointRoute}/{id}/checkin";
+
+        var response = await client.PutAsJsonAsync(uri, request);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
