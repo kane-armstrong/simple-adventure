9f0cdfc4abefb2b66048e08bc7d5449366377da8
Kane Armstrong <kane.armstrong@outlook.com>
2022-02-17 21:30:46 +1300

    Fix 500s caused by missing auth middleware

--- code/api/src/PetDoctor.API/Startup.cs
+++ code/api/src/PetDoctor.API/Startup.cs
@@ -160,6 +160,9 @@ public class Startup
 
         app.UseRouting();
 
+        app.UseAuthentication();
+        app.UseAuthorization();
+
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
