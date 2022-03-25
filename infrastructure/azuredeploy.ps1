92bbe1bd7c7903ef503137c61576ec5818928042
Kane Armstrong <kane.armstrong@outlook.com>
2022-03-25 16:09:30 +1300

    Fix script, make variables parameters

--- infrastructure/azuredeploy.ps1
+++ infrastructure/azuredeploy.ps1
@@ -1,9 +1,11 @@
-$location = "australiasoutheast"
-$environmentCode = "dev"
-$resourceGroupName = "pd-$environmentCode-rg"
+Param(
+    $location = "australiasoutheast",
+    $environmentCode = "dev",
+    $resourceGroupName = "pd-$environmentCode-rg"
+)
 
 az group create --name $resourceGroupName --location $location
 az deployment group create `
     --resource-group $resourceGroupName `
-    --template-file .\azuredeploy.bicep `
-    --parameters .\azuredeploy.parameters.json
+    --template-file .\shared\azuredeploy.bicep `
+    --parameters .\shared\azuredeploy.parameters.json
